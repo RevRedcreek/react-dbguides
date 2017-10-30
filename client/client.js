e01646e8143328844e6607c08f650f6bb1b95e51
Robert Esterlein <Bob@Roberts-MacBook-Pro.local>
2017-10-29 21:45:04 -0600

    try to set port to dyno env var

--- client/client.js
+++ client/client.js
@@ -11,11 +11,65 @@ import AdventureInfo from '../components/AdventureInfo'
 import AdventureInfoExpanded from '../components/AdventureInfoExpanded'
 
 import configureStore from '../redux/store'
-import {initialState} from '../redux/initialState'
+
 import  {Provider} from 'react-redux'
 import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 //configure and create store here
+
+let initialState = {
+  auth:{
+    isFetching: false,
+    isAuthenticated: true,
+    user: {
+      email: 'redcreek@yahoo.com',
+      password: '',
+      id: 21
+    }
+  },
+  guides: {
+    isFetching: false,
+    list: [
+      {
+        username: 'Elissa',
+        email: 'email',
+        id: 1,
+        avatar: '/assets/images/guides/header-1.jpg',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
+        expanded: false
+      },
+      {
+        username: 'Chris',
+        email: 'email',
+        id: 2,
+        avatar: '/assets/images/guides/header-2.jpg',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. '
+
+      }
+    ]
+  },
+  adventures : {
+    isFetching: false,
+    list: [
+      {
+        location: 'gunnison',
+        startTime: '2017-08-01',
+        endTime: '2017-08-01',
+        activity: 'Rock Climbing',
+        description:'Rock climb',
+        id: 1
+     },
+     {
+       location: 'lake city',
+       startTime: '2017-08-01',
+       endTime: '2017-08-01',
+       activity: 'Ice Climbing',
+       description:'Ice climb',
+       id: 2
+    }
+  ]
+  }
+}
 
 let store = configureStore(initialState)
 console.log( store )
